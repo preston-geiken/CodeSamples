2ede7186f37431386ba9e4b74b9cbc17e9d1b8d0
Preston-Geiken <prestongeiken@gmail.com>
2017-03-31 23:32:34 -0700

    ActivityLog Files Added

---
+++ ActivityLog/MidTier/ActivityTypeId.cs
@@ -0,0 +1,21 @@
+﻿namespace Sabio.Web.Enums
+{
+    public enum ActivityTypeId:int
+    {
+        NewAccount = 1,
+        AccountLogin = 2,
+        MadePayment = 3,
+        CreatedJob = 4,
+        JobUpdated = 5,
+        JobDeleted = 6,
+        ProfileUpdated = 7,
+        PasswordChanged = 8,
+        BringgTest = 9,
+        BringgTaskStatusUpdated =10,
+        Credits = 11,
+        CreditsFriend = 12
+
+
+
+    }
+}

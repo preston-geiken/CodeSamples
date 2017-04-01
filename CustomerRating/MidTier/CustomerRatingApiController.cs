4f296a26cddd3def38356f8f6472dac2c5e4ae9f
Preston-Geiken <prestongeiken@gmail.com>
2017-03-31 22:25:44 -0700

    Added Customer Rating

---
+++ CustomerRating/MidTier/CustomerRatingApiController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Practices.Unity;
+using Bringpro.Web.Domain;
+using Bringpro.Web.Models.Requests;
+using Bringpro.Web.Models.Responses;
+using Bringpro.Web.Services;
+using Bringpro.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bringpro.Web.Controllers.Api
+{
+    [RoutePrefix("api/customerrating")]
+    public class CustomerRatingApiController : ApiController
+    {
+        [Dependency]
+        public ICustomerRatingService _RatingService { get; set; }
+
+        [Route(), HttpPost]
+        public HttpResponseMessage Insert(CustomerRatingAddRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            ItemResponse<int> response = new ItemResponse<int>();
+
+            //response.Item = CustomerRatingService.Insert(model);
+            response.Item = _RatingService.Insert(model);
+
+
+            return Request.CreateResponse(response);
+
+        }
+
+        [Route(), HttpGet]
+        public HttpResponseMessage List()
+        {
+            ItemsResponse<CustomerRating> response = new ItemsResponse<CustomerRating>();
+
+            //response.Items = CustomerRatingService.GetAll();
+            response.Items = _RatingService.GetAll();
+
+
+            return Request.CreateResponse(response);
+        }
+    }
+}
---
+++ CustomerRating/MidTier/CustomerRatingService.cs
@@ -0,0 +1,74 @@
+﻿using Bringpro.Data;
+using Bringpro.Web.Domain;
+using Bringpro.Web.Models.Requests;
+using Bringpro.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Bringpro.Web.Services
+{
+    public class CustomerRatingService : BaseService, ICustomerRatingService 
+    {
+        public int Insert(CustomerRatingAddRequest model)
+        {
+            int Id = 0;
+
+            DataProvider.ExecuteNonQuery(GetConnection, "dbo.CustomerRating_Insert",
+                inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                {
+                    paramCollection.AddWithValue("@UserId", model.UserId);
+                    paramCollection.AddWithValue("@Rating", model.Rating);
+                    paramCollection.AddWithValue("@Note", model.Note);
+                    paramCollection.AddWithValue("@JobId", model.JobId);
+
+
+                    SqlParameter p = new SqlParameter("@Id", SqlDbType.Int);
+                    p.Direction = System.Data.ParameterDirection.Output;
+                    paramCollection.Add(p);
+                },
+
+                returnParameters: delegate (SqlParameterCollection param)
+                {
+                    int.TryParse(param["@Id"].Value.ToString(), out Id);
+                });
+
+            return Id;
+        }
+        public List<CustomerRating> GetAll()
+        {
+            List<CustomerRating> list = new List<CustomerRating>();
+
+            DataProvider.ExecuteCmd(GetConnection, "dbo.CustomerRating_SelectAll"
+              , inputParamMapper: null
+              , map: delegate (IDataReader reader, short set)
+              {
+                  CustomerRating c = new CustomerRating();
+                  int startingIndex = 0; //startingOrdinal
+
+                  c.Id = reader.GetSafeInt32(startingIndex++);
+                  c.UserId = reader.GetSafeString(startingIndex++);
+                  c.DateCreated = reader.GetSafeDateTime(startingIndex++);
+                  c.DateModified = reader.GetSafeDateTime(startingIndex++);
+                  c.Rating = reader.GetSafeDecimal(startingIndex++);
+                  c.Note = reader.GetSafeString(startingIndex++);
+                  c.JobId = reader.GetSafeInt32(startingIndex++);
+                  c.FirstName = reader.GetSafeString(startingIndex++);
+                  c.LastName = reader.GetSafeString(startingIndex++);
+                  c.ExternalUserId = reader.GetSafeString(startingIndex++);
+
+
+                  if (list == null)
+                  {
+                      list = new List<CustomerRating>();
+                  }
+                  list.Add(c);
+              });
+
+            return list;
+        }
+    }
+}

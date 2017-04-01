c89326aefc8801f85510a4341866e3d5e4251490
preston-geiken <prestongeiken@gmail.com>
2017-04-01 00:18:15 -0700

    add one file

--- ActivityLog/MidTier/SettingsViewModel.cs
+++ ActivityLog/MidTier/SettingsViewModel.cs
@@ -1,10 +1,10 @@
-﻿using Sabio.Web.Enums;
+﻿using Bringpro.Web.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Models.ViewModels
+namespace Bringpro.Web.Models.ViewModels
 {
     public class SettingsViewModel: BaseViewModel
     {
